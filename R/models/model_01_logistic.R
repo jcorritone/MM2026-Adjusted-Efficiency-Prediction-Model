6042e58b69fabafa9887b7cae94287710ce8de54
Joseph Corritone <jcorritone@gmail.com>
2026-03-16 18:02:23 +0100

    Create model_01_logistic.R

---
+++ R/models/model_01_logistic.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+model_01_logistic <- function(matchup_training_data) {
+
+  model_data <- matchup_training_data %>%
+    select(
+      Outcome,
+      SeedDiff,
+      WinPctDiff,
+      AvgPointDiffDiff) %>%
+    drop_na()
+
+  fit <- glm(
+    Outcome ~ SeedDiff + WinPctDiff + AvgPointDiffDiff,
+    data = model_data,
+    family = binomial(link = "logit"))
+
+  return(fit)
+}
