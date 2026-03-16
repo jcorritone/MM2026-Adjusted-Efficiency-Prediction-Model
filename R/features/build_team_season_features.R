487e0812839776d941157cf4f944754f6137ba4d
Joseph Corritone <jcorritone@gmail.com>
2026-03-16 20:21:31 +0100

    Update build_team_season_features.R

--- R/features/build_team_season_features.R
+++ R/features/build_team_season_features.R
@@ -2,17 +2,19 @@ library(tidyverse)
 
 build_team_season_features <- function(team_games) {
 
-  team_games %>%
+  team_games <- team_games %>%
     mutate(
       TeamPossessions = TeamFGA - TeamOR + TeamTO + 0.44 * TeamFTA,
       OppPossessions = OppFGA - OppOR + OppTO + 0.44 * OppFTA,
-      AdjTeamPossessions = TeamPossessions * (40 / (40 + 5 * NumOT)),
-      AdjOppPossessions = OppPossessions * (40 / (40 + 5 * NumOT)),
+      TeamAdjPossessions = TeamPossessions * (40 / (40 + 5 * NumOT)),
+      OppAdjPossessions = OppPossessions * (40 / (40 + 5 * NumOT)),
       OffEff = TeamScore / TeamPossessions,
       DefEff = OppScore / OppPossessions,
-      NetEff = OffEff - DefEff) %>%
+      NetEff = OffEff - DefEff)
   
+    team_season_features <- team_games %>%
     group_by(Season, TeamID) %>%
+    
     summarise(
       GamesPlayed = n(),
       Wins = sum(Win),
@@ -20,11 +22,28 @@ build_team_season_features <- function(team_games) {
       AvgTeamScore = mean(TeamScore),
       AvgOppScore = mean(OppScore),
       AvgPointDiff = mean(PointDiff),
-      AvgPossessions = mean(TeamPossessions),
+      AdjTempo = mean((TeamAdjPossessions + OppAdjPossessions) / 2),
       OffEff = mean(OffEff),
       DefEff = mean(DefEff),
       NetEff = mean(NetEff),
-      .groups = "drop") %>%
-  
-    arrange(Season, TeamID)
+      .groups = "drop")
+
+    sos <- team_games %>%
+    select(Season, TeamID, OpponentID) %>%
+    
+    left_join(
+      team_season_features %>%
+        select(Season, TeamID, OppNetEff = NetEff),
+      by = c("Season", "OpponentID" = "TeamID")) %>%
+    
+    group_by(Season, TeamID) %>%
+    
+    summarise(
+      SOS = mean(OppNetEff, na.rm = TRUE),
+      .groups = "drop")
+
+  team_season_features <- team_season_features %>%
+    left_join(sos, by = c("Season", "TeamID"))
+
+  return(team_season_features)
 }
