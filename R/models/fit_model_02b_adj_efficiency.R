66a1f43001eb5ac1638f0eaebab11f82a8737b02
Joseph Corritone <jcorritone@gmail.com>
2026-03-18 22:12:06 +0100

    Create fit_model_02b_adj_efficiency.R

---
+++ R/models/fit_model_02b_adj_efficiency.R
@@ -0,0 +1,18 @@
+fit_model_02b_adj_efficiency <- function(train_data) {
+
+  model_data <- train_data %>%
+    select(
+      Outcome,
+      SeedDiff,
+      AdjNetEffDiff,
+      AdjSOSDiff,
+      AdjTempoDiff
+    ) %>%
+    drop_na()
+
+  glm(
+    Outcome ~ SeedDiff + AdjNetEffDiff + AdjSOSDiff + AdjTempoDiff,
+    data = model_data,
+    family = binomial(link = "logit")
+  )
+}
